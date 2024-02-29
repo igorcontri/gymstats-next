5d1314d0554568c5062bc9071302fbe078015775
Igor Contri <igorcontri@gmail.com>
2024-02-29 13:47:23 -0300

    fix(*): alias "@/*" and "src/app" to ""app/"

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,48 @@
 import UserNavBar from "../../components/UserNavBar";
-import Card form 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Register() {
   return (
     <div className="defaultContainer flex flex-col justify-center items-center">
       <UserNavBar />
-      <div className="text-gray-50">
-        <Card />
+      <div className="text-white">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Create project</CardTitle>
+            <CardDescription>
+              Deploy your new project in one-click.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Name</Label>
+                  <Input
+                    id="name"
+                    placeholder="Name of your project"
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="framework">Framework</Label>
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="outline">Cancel</Button>
+            <Button>Deploy</Button>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
--- components/Card/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Card() {
-  return <div className="border w-[350px] h-screen mt-2 rounded-md "></div>;
-}
